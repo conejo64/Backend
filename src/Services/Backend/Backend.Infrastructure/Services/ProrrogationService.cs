5c1f4c4279e8e30c2b6cfb98be936d3c6a4c22f8
Jonathan Alexander Ortiz Najera <jonathan.ortiz@citytech.ec>
2023-05-01 19:20:16 -0500

    AGREGAR CASO ABIERTO POR DEFECTO

---
+++ src/Services/Backend/Backend.Infrastructure/Services/ProrrogationService.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Infrastructure.Services
+{
+    public class ProrrogationService : IProrrogationService
+    {
+        public async Task<bool> ProcessProrogation(Guid id, DateTime newDate)
+        {
+
+            return true;
+        }
+    }
+}
