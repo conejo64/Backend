c877b79297b9c4d77770fe47d5bb4a58467f2b74
Oscar Cedeno <ocedeno@ec.krugercorp.com>
2023-05-03 11:02:43 -0500

    Cambios en exportar excel

--- src/Services/Backend/Backend.API/DTOs/Requests/CaseRequests/ReadCasesToExcelRequest.cs
+++ src/Services/Backend/Backend.API/DTOs/Requests/CaseRequests/ReadCasesToExcelRequest.cs
@@ -3,7 +3,7 @@ using Shared.API.DTOs;
 
 namespace Backend.API.DTOs.Requests.CaseRequests
 {
-    public class ReadCasesToExcelRequest : BaseFilterDto
+    public class ReadCasesToExcelRequest
     {
         public Guid? BrandId { get; set; }
         public Guid? CaseStatusId { get; set; }
@@ -13,7 +13,7 @@ namespace Backend.API.DTOs.Requests.CaseRequests
 
         public ReadCasesToExcelQuery ToApplicationRequest()
         {
-            return new ReadCasesToExcelQuery(BrandId, CaseStatusId, DepartmentId, InitialDate, FinalDate, LoadChildren, IsPagingEnabled, Page, PageSize);
+            return new ReadCasesToExcelQuery(BrandId, CaseStatusId, DepartmentId, InitialDate, FinalDate);
         }
     }
 }
