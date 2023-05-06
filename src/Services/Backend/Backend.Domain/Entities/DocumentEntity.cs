51f1de5f457b5d982919a12a48d02a645d4bd74c
Jonathan Alexander Ortiz Najera <jonathan.ortiz@citytech.ec>
2023-05-06 11:42:52 -0500

    Requerimientos Diners

---
+++ src/Services/Backend/Backend.Domain/Entities/DocumentEntity.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Entities
+{
+    public class DocumentEntity : BaseEntity
+    {
+        public string Status { get; set; } = CatalogsStatus.Active;
+    }
+}
